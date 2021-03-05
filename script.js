c1b4a0b512b11ebe676cedd86de1bc2c465eed58
Sarfaraz45 <sarfaraz45ali@gmail.com>
2021-03-05 15:55:31 +0500

    JavaScript Assignment

---
+++ script.js
@@ -0,0 +1,145 @@
+//1. -- alert ("Welcome to Javascript Land... \n Happy Coding!") 
+
+
+// 2. ---var name = 'John Doe'
+// var myAge = 45 + " " +  "Years Old"
+// var profession = "Certified Mobile Application Developer"
+
+// alert(name);
+// alert(myAge);
+// alert(profession);
+
+
+// 3- var email = "sarfaraz45ali@gmail.com"
+// alert("My Email address is " +" " + email)
+
+// 4-  document.write("Yah!! I can write HTML content through javascript")
+
+
+// 5-  var age = 32;
+// alert('I am ' + " " + age + " " + 'years old')
+
+// 6 - var birthYear = 1990
+//alert("My BirthYear is  " +  "" + birthYear + "\nData type of my declared variable is" + " " +  typeof(birthYear) )
+
+
+
+// 7- document.write("<br>")
+// document.write("Variables names can contain only, number, $ and _, For Example: $my_1stVariable")
+// document.write("<br>")
+// document.write("Variable must begin with a letter, $ or _, For Example: $name, _name or name")
+// document.write("<br>")
+// document.write("Variable names are case sensitvie")
+// document.write("<br>")
+// document.write("Variable names shoudld not be JS keywords")
+
+
+// 8 and 9- var a = 30;
+// var b = 7;
+// document.write("Sum of " + a + " " + "and" +" " + b + " " + "is" +" "+ (a + b));
+// document.write("<br>")
+// document.write("Subtraction of " + a + " " + "and" +" " + b + " " + "is" +" "+ (a - b));
+// document.write("<br>")
+// document.write("Multiplication of " + a + " " + "and" +" " + b + " " + "is" +" "+ (a * b));
+// document.write("<br>")
+// document.write("Division of " + a + " " + "with" +" " + b + " " + "is" +" "+ (a / b));
+// document.write("<br>")
+// document.write("Modulus of " + a + " " + "with" +" " + b + " " + "is" +" "+ (a % b));
+
+
+// 10- var a;
+// document.write("Value after declaration is " + " " + a);
+// document.write("<br>")
+// a = 5;
+// document.write("Initial Value" + " " + a);
+// a = ++a
+// document.write("<br>")
+// document.write("Value after increment is " + " " + a);
+
+// a = a + 7;
+// document.write("<br>")
+// document.write("Value after addition is " + " " + a);
+
+// a = --a;
+// document.write("<br>")
+// document.write("Value ater decrement is "+ " "+ a);
+
+// a = a % 3
+// document.write("<br>")
+// document.write("OutPut:" + " " + '"',"The remainder is "+ " : " + a + '"');
+
+
+
+// 11- var celTemp = 25;
+// var celToFeh = (celTemp * 9 / 5) + 32
+// document.write(celTemp + "&degC is "+ celToFeh + "&degF");
+
+// var fTemp = 70;
+// var fTemptoCel = (fTemp - 32)  * 5 / 9
+// document.write("<br>")
+// document.write(fTemp + "&degF is "+ fTemptoCel + "&degC");
+
+// var USdollars = 10;
+// var sRial = 25
+// document.write("Total Currency in PKR: " + " " + ((USdollars*155) + (sRial*41)))
+
+// var a = 10;
+  //12
+// document.write("The Value of a is " + " " + a);
+// document.write("<h4> ....................................................</h4>")
+// document.write("<br>")
+// document.write("<br>")
+
+// a = ++a
+// document.write("The Value of ++a is: " + " " + a);
+// document.write("<br>")
+
+// document.write("Now The Value of a is: " + " " + a);
+
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
+
+
+// document.write("The Value of a++ is: " + " " + a++);
+
+// document.write("<br>")
+
+// document.write("Now The Value of a is: " + " " + a);
+
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
+
+
+// document.write("The Value of --a is: " + " " + --a);
+
+// document.write("<br>")
+
+// document.write("The Value of --a is: " + " " + a);
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
+
+
+
+// document.write("The Value of a-- is: " + " " + a--);
+// document.write("<br>")
+
+// document.write("Now The Value of a is: " + " " + a);
+// document.write("<h2> ----------------------------------------------------------------------------</h2>")
+
+//14
+// var a =2;
+// var b = 1;   1 - 0 + 1 + 1
+// var result = --a - --b + ++b +b--;
+
+
+// document.write(result);
+// document.write("<br>")
+// document.write(a);
+// document.write("<br>")
+// document.write(b);
+
+
+
